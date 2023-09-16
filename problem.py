25c1934ba705aa68225c2de32c4ac41d7e9e799e
jaume2000 <jaumeig95@gmail.com>
2023-09-16 16:32:18 +0200

    first commit

---
+++ problem.py
@@ -0,0 +1,10 @@
+
+class Line: 
+    def __init__(self, x1,y1, x2,y2):
+        self.p1 = (x1,y1)
+        self.p2 = (x2,y2)
+
+    def intersect_lines(self, other):
+        
+
+        return False
